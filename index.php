a39b1ae9d74c20d89be7836e1bc8e52d3a23fb0d
Devon Hess <DevonAlexanderHess@gmail.com>
2020-11-02 10:23:02 -0500

    add comments

--- index.php
+++ index.php
@@ -5,19 +5,23 @@
 	$files = [];
 	$list = [];
 
+	// Find appropriate image based on directory and filename
 	function grabImage($d, $f)
 	{
 		global $dir;
 		$ext = ["gif", "jpeg", "jpg", "png"];
 		for ($i = 0; $i < count($ext); $i++)
 		{
+			// If "file.mp3.png" exists
 			if (file_exists($d . $f . "." . $ext[$i]))
 			{
 				$img = $d . $f . "." . $ext[$i];
 				break;
 			}
+			// If "image.png" exists
 			else if (file_exists($d . "image." . $ext[$i]))
 			{
+				// If file is inside root folder
 				if ($dir == $d)
 				{
 					$img = '';
@@ -36,12 +40,13 @@
 		return $img;
 	}
 
-	function addEntry($t, $s, $l, $g, $p, $i)
+	// Add an RSS item (title, description, link, guid, date)
+	function addEntry($t, $d, $l, $g, $p, $i)
 	{
 		global $path;
 		echo "<item>";
 		echo "<title>" . $t . "</title>";
-		echo "<description>" . $s . "</description>";
+		echo "<description>" . $d . "</description>";
 		echo "<link>" . $l . "</link>";
 		echo "<guid>" . $g . "</guid>";
 		echo "<pubDate>" . $p . "</pubDate>";
@@ -52,6 +57,7 @@
 		echo "</item>";
 	}
 	
+	// Create list of all files inside folders and subfolders
 	function recurse($d)
 	{
 		global $path, $list;
@@ -79,6 +85,7 @@
 		}
 	}
 
+	// Create XML header
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 	echo "<rss version=\"2.0\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\">";
 	echo "<channel>";
@@ -88,17 +95,21 @@
 	echo "<itunes:image href=\"" . $path . grabImage($dir, "image") . "\"/>";
 	echo "<itunes:block>yes</itunes:block>";
 	
+	// Create file list starting at root directory
 	recurse($dir);
 
+	// Sort files in reverse order to try to match RSS convention
 	usort($list, function ($a, $b)
 	{
 		return strnatcasecmp($b->dir . $b->file, $a->dir . $a->file);
 	});
 
+	// Iterate through file list and create XML entries
 	for ($i = 0; $i < count($list); $i++)
 	{
 		$ext = preg_replace('/.*(?:\.(.*))/','$1',strtolower($list[$i]->file));
 
+		// Ignore unsupported file types
 		switch ($ext)
 		{
 			case 'flac':
@@ -127,6 +138,7 @@
 		);
 	}
 
+	// Create XML footer
 	echo "</channel>";
 	echo "</rss>";
 ?>
